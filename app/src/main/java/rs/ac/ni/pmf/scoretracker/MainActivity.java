afd1b7a681f5409a10bc8428988613535a425ccc
Marko Milosevic <ninja643@gmail.com>
2020-04-07 13:58:45 +0200

    Initial commit

---
+++ app/src/main/java/rs/ac/ni/pmf/scoretracker/MainActivity.java
@@ -0,0 +1,16 @@
+package rs.ac.ni.pmf.scoretracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
