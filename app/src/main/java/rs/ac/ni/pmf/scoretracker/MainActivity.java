56887d77f4fceb79f6e19ad52e69b00140c1f878
Marko Milosevic <ninja643@gmail.com>
2020-05-02 00:23:36 +0200

    Remove comments

--- app/src/main/java/rs/ac/ni/pmf/scoretracker/MainActivity.java
+++ app/src/main/java/rs/ac/ni/pmf/scoretracker/MainActivity.java
@@ -5,32 +5,16 @@ import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.lifecycle.ViewModelProvider;
 
 public class MainActivity extends AppCompatActivity implements GamesListFragment.OnGameSelectedListener
 {
 	private static final String TAG = "SCORE_TRACKER";
-
-//	private ScoreViewModel scoreViewModel;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-//		scoreViewModel = new ViewModelProvider(this).get(ScoreViewModel.class);
-
-//		final MutableLiveData<Integer> currentIndex = scoreViewModel.getCurrentIndex();
-//		currentIndex.observe(this, new Observer<Integer>()
-//		{
-//			@Override
-//			public void onChanged(final Integer index)
-//			{
-//				Log.i(TAG, "Index from main: " + index);
-//				onGameSelected(index);
-//			}
-//		});
 
 		if (findViewById(R.id.portrait_view_container) != null)
 		{
@@ -49,10 +33,6 @@ public class MainActivity extends AppCompatActivity implements GamesListFragment
 	@Override
 	public void onGameSelected(final int gameIndex)
 	{
-//		final CurrentGameFragment currentGameFragment =
-//				(CurrentGameFragment) getSupportFragmentManager()
-//						.findFragmentById(R.id.current_game_fragment);
-//
 		if (findViewById(R.id.portrait_view_container) != null)
 		{
 			Log.i(TAG, "Creating game fragment");
